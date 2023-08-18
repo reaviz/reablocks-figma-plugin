8cd66dec513a965e8fca59be8fbfff87e5159a16
mohit tater <mohittater.iiita@gmail.com>
2023-08-18 17:28:46 +0530

    change color tokens to lowercase, some other minor refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,57 @@
-import { emit, once, showUI } from "@create-figma-plugin/utilities";
-import chroma from "chroma-js";
+import { emit, on, showUI } from '@create-figma-plugin/utilities';
+import chroma from 'chroma-js';
+
+interface ColorTokenInfo {
+  primaryLabel: string;
+  secondaryLabel: string;
+  fullName: string;
+  opacity: number | undefined;
+  hex: string;
+}
+
+interface CSSJson {
+  colors: { [k: string]: any };
+}
 
 export default function () {
-  function generateCSS() {
+  function generateCSS(): CSSJson {
     const localStyles = figma.getLocalPaintStyles();
-    const requiredStylesElements = [];
+    const requiredStylesElements: ColorTokenInfo[] = [];
+    let colorJson: { [k: string]: any } = {};
 
     for (const style of localStyles) {
-      if (style.type === "PAINT" && style.paints[0].type === "SOLID") {
+      if (style.type === 'PAINT' && style.paints[0].type === 'SOLID') {
         // Example: Split input string like White, 'Primary/100', 'Chart/Chart Red 100' into
         // ['White', undefined], ['Primary', '100'], ['Chart', 'Chart Red 100'] while handling optional secondary value
-        const [primaryLabel, secondaryLabel] = style.name.split("/").map((part) => part.trim());
+        const [primaryLabel, secondaryLabel] = style.name
+          .split("/")
+          .map((part) => part.trim().toLowerCase());
         const { r, g, b } = (style.paints[0] as SolidPaint).color;
-        requiredStylesElements.push({
-          fullName: style.name,
-          primaryLabel,
-          secondaryLabel,
-          opacity: style.paints[0].opacity,
-          hex: chroma
-            .rgb(
-              r * 255,
-              g * 255,
-              b * 255,
-              style.paints[0].opacity ?? 1
-            )
-            .hex(),
-        });
-      }
-    }
-
-    let colorJson: { [k: string]: any } = {};
-    for (const color of requiredStylesElements) {
-      const { primaryLabel, secondaryLabel, hex } = color;
-
-      if (primaryLabel) {
-        if (secondaryLabel) {
-          colorJson[primaryLabel] = {
-            ...colorJson[primaryLabel],
-            [secondaryLabel]: hex,
-          };
-        } else {
-          colorJson[primaryLabel] = hex;
+        const hex = chroma.rgb(r * 255, g * 255, b * 255, style.paints[0].opacity ?? 1).hex();
+        if (primaryLabel) {
+          if (secondaryLabel) {
+            colorJson[primaryLabel] = {
+              ...colorJson[primaryLabel],
+              [secondaryLabel]: hex,
+            };
+          } else {
+            colorJson[primaryLabel] = hex;
+          }
         }
       }
     }
 
-    return colorJson;
+    return {
+      'colors': colorJson
+    };
   }
 
   /**
    * Msg handlers
    */
-  once("GENERATE_CSS", () => {
+  on('GENERATE_CSS', () => {
     const cssObj = generateCSS();
-    emit("SUCCESS", { value: cssObj });
+    emit('SUCCESS', { value: cssObj });
   });
 
   showUI({ height: 300, width: 600 });
