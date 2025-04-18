f46b398bf684242183be7fea9ad66fd08e5d5292
Evgeniy <evgeniy@goodcode.us>
2025-04-18 16:42:42 +0300

    Export CSS variables

--- src/ui.tsx
+++ src/ui.tsx
@@ -4,82 +4,133 @@ import {
   Button,
   Columns,
   Container,
+  Dropdown,
   render,
-  VerticalSpace,
-  TextboxMultiline
+  TextboxMultiline,
+  VerticalSpace
 } from '@create-figma-plugin/ui';
-import { emit, once } from '@create-figma-plugin/utilities';
-import { h } from 'preact';
-import { useCallback, useState, useEffect } from 'preact/hooks';
-import styles from './styles.css';
+import { emit, on, once } from '@create-figma-plugin/utilities';
 import copy from 'copy-to-clipboard';
+import { h } from 'preact';
+import { useCallback, useEffect, useState } from 'preact/hooks';
+import styles from './styles.css';
 
 function Plugin() {
-  const [colorPalette, setColorPalette] = useState(`Click on Generate CSS!`);
-  const [themeTokens, setThemeTokens] = useState(`Click on Generate CSS!`);
+  const [colorVariables, setColorVariables] = useState('');
+  const [modeVariables, setModeVariables] = useState('');
+  const [componentVariables, setComponentVariables] = useState('');
+  const [modes, setModes] = useState<Record<string, string>>({});
+  const [selectedMode, setSelectedMode] = useState<string | null>(null);
 
-  const generateCSSButtonClick = useCallback(function (): void {
-    emit('GENERATE_CSS');
+  const generateColorVariablesClickHandler = useCallback(() => {
+    emit('GENERATE_COLOR_VARIABLES');
+    setColorVariables('Generating...')
   }, []);
 
-  useEffect(() => {
-    once('SUCCESS', (args) => {
-      const { colors, themes } = args.value;
-      setColorPalette(JSON.stringify(colors, null, 2));
+  const generateModeVariablesClickHandler = useCallback(() => {
+    emit('GENERATE_MODE_VARIABLES', { mode: selectedMode });
+    setModeVariables('Generating...');
+  }, [selectedMode]);
 
-      // remove quotes from all colorPalette aliases
-      // ie, "colorPalette.blue[500]" =>  colorPalette.blue[500]
-      const regex = /"colorPalette\.(.+)"/g;
-      const formatted = JSON.stringify(themes, null, 2).replace(
-        regex,
-        (match) => match.replace(/"/g, '')
-      );
-      setThemeTokens(formatted);
+  const generateComponentVariablesClickHandler = useCallback(() => {
+    emit('GENERATE_COMPONENT_VARIABLES');
+    setComponentVariables('Generating...');
+  }, []);
+
+  const copyColors = useCallback(() => {
+    copy(colorVariables);
+  }, [colorVariables])
+
+  const copyModeTokens = useCallback(() => {
+    copy(modeVariables);
+  }, [modeVariables]);
+
+  const copyTokens = useCallback(() => {
+    copy(componentVariables);
+  }, [componentVariables]);
+
+  useEffect(() => {
+    emit('LOAD_MODES');
+    once('LOADED_MODES', ({ modes }) => {
+      setModes(modes);
+      setSelectedMode(Object.keys(modes)?.[0])
+    });
+    on('GENERATED_COLOR_VARIABLES', ({ tokens }: {tokens: { token: string, value: string}[]}) => {
+      setColorVariables(tokens.map(({ token, value }) => `--${token}: ${value};`).join(`\n`))
+    });
+    on('GENERATED_MODE_VARIABLES', ({ tokens }: {tokens: { token: string, value: string}[]}) => {
+      setModeVariables(tokens.map(({ token, value }) => `--${token}: var(--${value});`).join(`\n`))
+    });
+    on('GENERATED_COMPONENT_VARIABLES', ({ tokens }: {tokens: { token: string, value: string}[]}) => {
+      setComponentVariables(tokens.map(({ token, value }) => `--color-${token}: var(--${value});`).join(`\n`))
     });
   }, []);
 
-  function copyColorPalette() {
-    copy(colorPalette);
-  }
-
-  function copyThemeTokens() {
-    copy(themeTokens);
-  }
-
   return (
     <Container space="large">
-      <VerticalSpace space="small" />
-      Color Palettes:
-      <div class={styles.container}>
-        <TextboxMultiline
-          rows={10}
-          placeholder="Click on Generate Tokens"
-          value={colorPalette}
-        />
-      </div>
+      Color variables:
       <VerticalSpace space="extraSmall" />
-      Themes:
       <div class={styles.container}>
         <TextboxMultiline
           rows={10}
-          placeholder="Click on Generate Tokens"
-          value={themeTokens}
+          placeholder="Click on Generate"
+          value={colorVariables}
         />
       </div>
-      <VerticalSpace space="small" />
-      <Button fullWidth onClick={generateCSSButtonClick}>
-        Generate Tokens
-      </Button>
       <VerticalSpace space="small" />
       <Columns space="small">
-        <Button fullWidth onClick={copyColorPalette}>
-          Copy Colors
+        <Button fullWidth onClick={generateColorVariablesClickHandler}>
+          Generate
         </Button>
-        <Button fullWidth onClick={copyThemeTokens}>
-          Copy Themes
+        <Button onClick={copyColors}>
+          Copy
         </Button>
       </Columns>
+      <VerticalSpace space="large" />
+      Mode variables:
+      <VerticalSpace space="extraSmall" />
+      <Dropdown
+        placeholder="Select Mode"
+        value={selectedMode}
+        options={Object.entries(modes).map(([key, val]) => ({text: val, value: key }))}
+        onChange={(e) => setSelectedMode(e.currentTarget.value)}
+      />
+      <VerticalSpace space="extraSmall" />
+      <div class={styles.container}>
+        <TextboxMultiline
+          rows={10}
+          placeholder="Click on Generate"
+          value={modeVariables}
+        />
+      </div>
       <VerticalSpace space="small" />
+      <Columns space="small">
+        <Button fullWidth onClick={generateModeVariablesClickHandler}>
+          Generate
+        </Button>
+        <Button onClick={copyModeTokens}>
+          Copy
+        </Button>
+      </Columns>
+      <VerticalSpace space="large" />
+      Component variables:
+      <VerticalSpace space="extraSmall" />
+      <div class={styles.container}>
+        <TextboxMultiline
+          rows={10}
+          placeholder="Click on Generate"
+          value={componentVariables}
+        />
+      </div>
+      <VerticalSpace space="small" />
+      <Columns space="small">
+        <Button fullWidth onClick={generateComponentVariablesClickHandler}>
+          Generate
+        </Button>
+        <Button onClick={copyTokens}>
+          Copy
+        </Button>
+      </Columns>
     </Container>
   );
 }
