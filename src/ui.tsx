d6fc68b4bf820460837ebfe1afb9e5f3dcb4ec12
Stephanie Yang <steph@goodcode.us>
2025-04-22 14:06:14 -0700

    tweaks to figma plugin UI

--- src/ui.tsx
+++ src/ui.tsx
@@ -6,7 +6,7 @@ import {
   Dropdown,
   render,
   TextboxMultiline,
-  VerticalSpace
+  VerticalSpace,
 } from '@create-figma-plugin/ui';
 import { emit, on, once } from '@create-figma-plugin/utilities';
 import copy from 'copy-to-clipboard';
@@ -26,14 +26,15 @@ function Plugin() {
   const [selectedMode, setSelectedMode] = useState<string | null>(null);
   const [colorsCopyButtonText, setColorsCopyButtonText] = useState(COPY_TITLE);
   const [modeCopyButtonText, setModeCopyButtonText] = useState(COPY_TITLE);
-  const [componentCopyButtonText, setComponentCopyButtonText] = useState(COPY_TITLE);
+  const [componentCopyButtonText, setComponentCopyButtonText] =
+    useState(COPY_TITLE);
 
   const generateVariablesHandler = useCallback(() => {
     setColorVariables(GENERATING_TITLE);
     setModeVariables(GENERATING_TITLE);
     setComponentVariables(GENERATING_TITLE);
     emit('GENERATE_COLOR_VARIABLES');
-    emit('GENERATE_MODE_VARIABLES', {mode: selectedMode});
+    emit('GENERATE_MODE_VARIABLES', { mode: selectedMode });
     emit('GENERATE_COMPONENT_VARIABLES');
   }, [selectedMode]);
 
@@ -41,7 +42,7 @@ function Plugin() {
     copy(colorVariables);
     setColorsCopyButtonText(COPIED_TITLE);
     setTimeout(() => setColorsCopyButtonText(COPY_TITLE), 2000);
-  }, [colorVariables])
+  }, [colorVariables]);
 
   const copyModeTokens = useCallback(() => {
     copy(modeVariables);
@@ -57,36 +58,61 @@ function Plugin() {
 
   useEffect(() => {
     emit('LOAD_MODES');
-    once('LOADED_MODES', ({modes}) => {
+    once('LOADED_MODES', ({ modes }) => {
       setModes(modes);
-      setSelectedMode(Object.keys(modes)?.[0])
+      setSelectedMode(Object.keys(modes)?.[0]);
     });
-    on('GENERATED_COLOR_VARIABLES', ({tokens}: { tokens: { token: string, value: string }[] }) => {
-      setColorVariables(tokens.map(({token, value}) => `--${token}: ${value};`).join(`\n`))
-    });
-    on('GENERATED_MODE_VARIABLES', ({tokens}: { tokens: { token: string, value: string }[] }) => {
-      setModeVariables(tokens.map(({token, value}) => `--${token}: var(--${value});`).join(`\n`))
-    });
-    on('GENERATED_COMPONENT_VARIABLES', ({tokens}: { tokens: { token: string, value: string }[] }) => {
-      setComponentVariables(tokens.map(({token, value}) => `--color-${token}: var(--${value});`).join(`\n`))
-    });
+    on(
+      'GENERATED_COLOR_VARIABLES',
+      ({ tokens }: { tokens: { token: string; value: string }[] }) => {
+        setColorVariables(
+          tokens.map(({ token, value }) => `--${token}: ${value};`).join(`\n`),
+        );
+      },
+    );
+    on(
+      'GENERATED_MODE_VARIABLES',
+      ({ tokens }: { tokens: { token: string; value: string }[] }) => {
+        setModeVariables(
+          tokens
+            .map(({ token, value }) => `--${token}: var(--${value});`)
+            .join(`\n`),
+        );
+      },
+    );
+    on(
+      'GENERATED_COMPONENT_VARIABLES',
+      ({ tokens }: { tokens: { token: string; value: string }[] }) => {
+        setComponentVariables(
+          tokens
+            .map(({ token, value }) => `--color-${token}: var(--${value});`)
+            .join(`\n`),
+        );
+      },
+    );
   }, [setModes, setSelectedMode]);
 
   return (
     <Container space="large">
+      <VerticalSpace space="small" />
+      Theme:
+      <VerticalSpace space="extraSmall" />
       <Dropdown
         placeholder="Select Mode"
         value={selectedMode}
-        options={Object.entries(modes).map(([key, val]) => ({text: val, value: key}))}
+        options={Object.entries(modes).map(([key, val]) => ({
+          text: val,
+          value: key,
+        }))}
         onChange={(e) => setSelectedMode(e.currentTarget.value)}
       />
-      <VerticalSpace space="small"/>
+      <VerticalSpace space="small" />
       <Button fullWidth onClick={generateVariablesHandler}>
         Generate
       </Button>
-      <VerticalSpace space="large"/>
+      <VerticalSpace space="large" />
       Color variables:
-      <VerticalSpace space="extraSmall"/>
+      <VerticalSpace space="extraSmall" />
       <div class={styles.container}>
         <TextboxMultiline
           rows={10}
@@ -94,16 +120,20 @@ function Plugin() {
           value={colorVariables}
         />
       </div>
-      <VerticalSpace space="small"/>
+      <VerticalSpace space="small" />
       <Button
-        disabled={!colorVariables || colorVariables === GENERATING_TITLE || colorsCopyButtonText === COPIED_TITLE}
+        disabled={
+          !colorVariables ||
+          colorVariables === GENERATING_TITLE ||
+          colorsCopyButtonText === COPIED_TITLE
+        }
         onClick={copyColors}
       >
         {colorsCopyButtonText}
       </Button>
-      <VerticalSpace space="large"/>
+      <VerticalSpace space="large" />
       Mode variables:
-      <VerticalSpace space="extraSmall"/>
+      <VerticalSpace space="extraSmall" />
       <div class={styles.container}>
         <TextboxMultiline
           rows={10}
@@ -111,16 +141,20 @@ function Plugin() {
           value={modeVariables}
         />
       </div>
-      <VerticalSpace space="small"/>
+      <VerticalSpace space="small" />
       <Button
-        disabled={!modeVariables || modeVariables === GENERATING_TITLE || modeCopyButtonText === COPIED_TITLE}
+        disabled={
+          !modeVariables ||
+          modeVariables === GENERATING_TITLE ||
+          modeCopyButtonText === COPIED_TITLE
+        }
         onClick={copyModeTokens}
       >
         {modeCopyButtonText}
       </Button>
-      <VerticalSpace space="large"/>
+      <VerticalSpace space="large" />
       Component variables:
-      <VerticalSpace space="extraSmall"/>
+      <VerticalSpace space="extraSmall" />
       <div class={styles.container}>
         <TextboxMultiline
           rows={10}
@@ -128,13 +162,18 @@ function Plugin() {
           value={componentVariables}
         />
       </div>
-      <VerticalSpace space="small"/>
+      <VerticalSpace space="small" />
       <Button
-        disabled={!componentVariables ||  componentVariables === GENERATING_TITLE || componentCopyButtonText === COPIED_TITLE}
+        disabled={
+          !componentVariables ||
+          componentVariables === GENERATING_TITLE ||
+          componentCopyButtonText === COPIED_TITLE
+        }
         onClick={copyTokens}
       >
         {componentCopyButtonText}
       </Button>
+      <VerticalSpace space="small" />
     </Container>
   );
 }
