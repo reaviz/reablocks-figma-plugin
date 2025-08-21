809b494ced8c55c813f530014297c114e5a75c20
Evgeniy <evgeniy@goodcode.us>
2025-08-21 14:45:49 +0300

    Non-color tokens export

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { emit, on, showUI } from '@create-figma-plugin/utilities';
 import { kebabCase } from 'change-case';
 import chroma from 'chroma-js';
 
-const COLORS_COLLECTION_NAME = 'Lvl 01 - Root'
+const ROOT_COLLECTION_NAME = 'Lvl 01 - Root'
+const STYLE_COLLECTION_NAME = 'Lvl 02 - Style'
 const MODE_COLLECTION_NAME = 'Lvl 03 (A) - Mode'
 const COMPONENTS_COLLECTION_NAME = 'Lvl 04 - Component'
 
@@ -38,7 +39,7 @@ export default function () {
   on('GENERATE_COLOR_VARIABLES', async () => {
     const tokens = [];
     const collections = await figma.variables.getLocalVariableCollectionsAsync();
-    const colorsCollection = collections.filter(({ name }) => name === COLORS_COLLECTION_NAME)?.[0];
+    const colorsCollection = collections.filter(({ name }) => name === ROOT_COLLECTION_NAME)?.[0];
     const mode = colorsCollection?.modes?.[0]?.modeId; // Only one mode is supported for Root level
 
     for (const variableId of colorsCollection?.variableIds || []) {
@@ -77,7 +78,7 @@ export default function () {
         const value = variable.valuesByMode[mode]
 
         if ((value as VariableAlias)?.type === 'VARIABLE_ALIAS') {
-          const targetVariable = await getNestedVariable((value as VariableAlias)?.id, COLORS_COLLECTION_NAME);
+          const targetVariable = await getNestedVariable((value as VariableAlias)?.id, ROOT_COLLECTION_NAME);
 
           tokens.push({
             token: name,
@@ -116,6 +117,136 @@ export default function () {
     }
 
     emit('GENERATED_COMPONENT_VARIABLES', { tokens });
+  });
+
+  on('GENERATE_OTHER_VARIABLES', async () => {
+    const tokens = [];
+    const collections = await figma.variables.getLocalVariableCollectionsAsync();
+    const stylesCollection = collections.filter(({ name }) => name === STYLE_COLLECTION_NAME)?.[0];
+    const rootCollection = collections.filter(({ name }) => name === ROOT_COLLECTION_NAME)?.[0];
+    const rootMode = rootCollection?.modes?.[0]?.modeId; // Only one mode is supported for Root level
+    const stylesMode = stylesCollection?.modes?.[0]?.modeId; // Only one mode is supported for Root level
+
+    let fontsStoredCount = 0;
+
+    tokens.push('/* Font Families */');
+    for (const variableId of rootCollection?.variableIds || []) {
+      const variable = await figma.variables.getVariableByIdAsync(variableId);
+
+      if (variable?.name?.startsWith('Typography/Family')) {
+        if (variable?.name?.toLowerCase()?.includes('mono')) {
+          tokens.push({
+            token: 'font-mono',
+            value: `"${variable?.valuesByMode[rootMode]}", monospace`
+          })
+        } else if (fontsStoredCount === 0) {
+          tokens.push({
+            token: 'font-sans',
+            value: `"${variable?.valuesByMode[rootMode]}", sans-serif`
+          });
+        } else if (fontsStoredCount === 1) {
+          tokens.push({
+            token: 'font-serif',
+            value: `"${variable?.valuesByMode[rootMode]}", serif`
+          });
+        }
+        fontsStoredCount++;
+      }
+    }
+
+    tokens.push('');
+    tokens.push('/* Font Sizes */');
+    for (const variableId of stylesCollection?.variableIds || []) {
+      const variable = await figma.variables.getVariableByIdAsync(variableId);
+
+      if (variable?.name?.startsWith('Typography/Font Size')) {
+        const name = variable?.name.split('/')?.pop()?.toLowerCase();
+        const value = variable.valuesByMode[stylesMode]
+
+        if ((value as VariableAlias)?.type === 'VARIABLE_ALIAS') {
+          const targetVariable = await getNestedVariable((value as VariableAlias)?.id, ROOT_COLLECTION_NAME);
+          const fontSize = targetVariable?.valuesByMode?.[rootMode] as number;
+          tokens.push({
+            token: `text-${name}`,
+            value: `${fontSize / 16}rem`,
+          })
+        }
+      }
+    }
+
+    tokens.push('');
+    tokens.push('/* Line Heights */');
+    for (const variableId of stylesCollection?.variableIds || []) {
+      const variable = await figma.variables.getVariableByIdAsync(variableId);
+
+      if (variable?.name?.startsWith('Typography/Line Height')) {
+        const name = variable?.name.split('/')?.pop()?.toLowerCase();
+        const value = variable.valuesByMode[stylesMode]
+
+        if ((value as VariableAlias)?.type === 'VARIABLE_ALIAS') {
+          const targetVariable = await getNestedVariable((value as VariableAlias)?.id, ROOT_COLLECTION_NAME);
+          const fontSize = targetVariable?.valuesByMode?.[rootMode] as number;
+          tokens.push({
+            token: `text-${name}--line-height`,
+            value: `${fontSize / 16}rem`,
+          })
+        }
+      }
+    }
+
+    tokens.push('');
+    tokens.push('/* Corner Radius */');
+    for (const variableId of stylesCollection?.variableIds || []) {
+      const variable = await figma.variables.getVariableByIdAsync(variableId);
+
+      if (variable?.name?.startsWith('Corner Radius')) {
+        const name = variable?.name.split('/')?.pop()?.toLowerCase();
+        const value = variable.valuesByMode[stylesMode]
+
+        if ((value as VariableAlias)?.type === 'VARIABLE_ALIAS') {
+          const targetVariable = await getNestedVariable((value as VariableAlias)?.id, ROOT_COLLECTION_NAME);
+          const fontSize = targetVariable?.valuesByMode?.[rootMode] as number;
+          tokens.push({
+            token: `radius-${name}`,
+            value: `${fontSize / 16}rem`,
+          })
+        }
+      }
+    }
+
+    tokens.push('');
+    tokens.push('/* Blur */');
+    for (const variableId of rootCollection?.variableIds || []) {
+      const variable = await figma.variables.getVariableByIdAsync(variableId);
+
+      if (variable?.name?.startsWith('Effects/Blur')) {
+        const name = variable?.name.split('/')?.pop()?.toLowerCase();
+        const value = variable.valuesByMode[rootMode] as number;
+
+        tokens.push({
+          token: `blur-${name}`,
+          value: `${value / 16}rem`,
+        })
+      }
+    }
+
+    tokens.push('');
+    tokens.push('/* Shadow */');
+    for (const variableId of rootCollection?.variableIds || []) {
+      const variable = await figma.variables.getVariableByIdAsync(variableId);
+
+      if (variable?.name?.startsWith('Effects/Shadow')) {
+        const name = variable?.name.split('/')?.pop()?.toLowerCase();
+        const value = variable.valuesByMode[rootMode] as number;
+
+        tokens.push({
+          token: `drop-shadow-${name}`,
+          value: `${value / 16}rem`,
+        })
+      }
+    }
+
+    emit('GENERATED_OTHER_VARIABLES', { tokens });
   });
 
   showUI({ height: 500, width: 400 });
